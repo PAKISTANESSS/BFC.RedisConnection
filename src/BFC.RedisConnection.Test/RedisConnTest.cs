aa6672b2f3c4583143d8696c5223b1273edc2db6
Bernardo Coelho <fcoelhobernardo@gmail.com>
2020-09-27 04:34:44 +0100

    Create RedisConnTest.cs

---
+++ src/BFC.RedisConnection.Test/RedisConnTest.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BFC.RedisConnection.Test
+//{
+//    [TestClass]
+//    public class RedisConnTest
+//    {
+//        [TestMethod]
+//        public void RedisConn_SetKey_Test()
+//        {
+
+//        }
+//    }
+//}
